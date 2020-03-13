cc95a206a3f1775ae7d3ff5c0cdcf5364fcea679
Jørgen Aleksander Fagervik <jorgen.fagervik@revolve.no>
2020-03-13 18:06:00 +0100

    Events, premake update and pch

--- Daevon/Daevon/Core.h
+++ Daevon/Daevon/Core.h
@@ -9,3 +9,5 @@
 #else
 	#error Daevon only supports Windows!
 #endif
+
+#define BIT(x) (1 << x)
--- premake5.lua
+++ premake5.lua
@@ -1,108 +1,150 @@
-
 workspace "Daevon"
-    architecture "x64"
+	architecture "x86_64"
+	startproject "Sandbox"
 
-    configurations
-    {
-        "Debug",
-        "Release",
-        "Dist"
-    }
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+	
+	flags
+	{
+		"MultiProcessorCompile"
+	}
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
+-- Include directories relative to root folder (solution directory)
+IncludeDir = {}
+IncludeDir["GLFW"] = "Daevon/vendor/GLFW/include"
+IncludeDir["Glad"] = "Daevon/vendor/Glad/include"
+IncludeDir["ImGui"] = "Daevon/vendor/imgui"
+IncludeDir["glm"] = "Daevon/vendor/glm"
+IncludeDir["stb_image"] = "Daevon/vendor/stb_image"
+
+group "Dependencies"
+	include "Daevon/vendor/GLFW"
+	include "Daevon/vendor/Glad"
+	include "Daevon/vendor/imgui"
+
+group ""
+
 project "Daevon"
-    location "Daevon"
-    kind "SharedLib"
-    language "C++"
+	location "Daevon"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
 
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-    files
-    {
-        "%{prj.name}/src/**.h",
-        "%{prj.name}/src/**.cpp"
-    }
+	pchheader "dvpch.h"
+	pchsource "Daevon/src/dvpch.cpp"
 
-    includedirs
-    {
-        "%{prj.name}/vendor/spdlog/include"
-    }
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+		"%{prj.name}/vendor/stb_image/**.h",
+		"%{prj.name}/vendor/stb_image/**.cpp",
+		"%{prj.name}/vendor/glm/glm/**.hpp",
+		"%{prj.name}/vendor/glm/glm/**.inl",
+	}
 
-    filter "system:windows"
-        cppdialect "C++17"
-        staticruntime "On"
-        systemversion "latest"
+	defines
+	{
+		"_CRT_SECURE_NO_WARNINGS"
+	}
 
-    defines
-    {
-        "DV_PLATFORM_WINDOWS",
-        "DV_BUILD_DLL"
-    }
+	includedirs
+	{
+		"%{prj.name}/src",
+		"%{prj.name}/vendor/spdlog/include",
+		"%{IncludeDir.GLFW}",
+		"%{IncludeDir.Glad}",
+		"%{IncludeDir.ImGui}",
+		"%{IncludeDir.glm}",
+		"%{IncludeDir.stb_image}"
+	}
 
-    postbuildcommands
-    {
-        ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
-    }
+	links 
+	{ 
+		"GLFW",
+		"Glad",
+		"ImGui",
+		"opengl32.lib"
+	}
 
-    filter "configurations:Debug"
-        defines "DV_DEBUG"
-        symbols "On"
+	filter "system:windows"
+		systemversion "latest"
 
-    filter "configurations:Release"
-        defines "DV_RELEASE"
-        optimize "On"
+		defines
+		{
+			"DV_BUILD_DLL",
+			"GLFW_INCLUDE_NONE"
+		}
 
-    filter "configurations:Dist"
-        defines "DV_DIST"
-        optimize "On"
+	filter "configurations:Debug"
+		defines "DV_DEBUG"
+		runtime "Debug"
+		symbols "on"
 
+	filter "configurations:Release"
+		defines "DV_RELEASE"
+		runtime "Release"
+		optimize "on"
 
-        
+	filter "configurations:Dist"
+		defines "DV_DIST"
+		runtime "Release"
+		optimize "on"
+
 project "Sandbox"
-    location "Sandbox"
-    kind "ConsoleApp"
-    language "C++"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
 
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-    files
-    {
-        "%{prj.name}/src/**.h",
-        "%{prj.name}/src/**.cpp"
-    }
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
 
-    includedirs
-    {
-        "Daevon/vendor/spdlog/include",
-        "Daevon/src"
-    }
+	includedirs
+	{
+		"Daevon/vendor/spdlog/include",
+		"Daevon/src",
+		"Daevon/vendor",
+		"%{IncludeDir.glm}"
+	}
 
-    links
-    {
-        "Daevon"
-    }
+	links
+	{
+		"Daevon"
+	}
 
-    filter "system:windows"
-        cppdialect "C++17"
-        staticruntime "On"
-        systemversion "latest"
+	filter "system:windows"
+		systemversion "latest"
+		
+	filter "configurations:Debug"
+		defines "DV_DEBUG"
+		runtime "Debug"
+		symbols "on"
 
-    defines
-    {
-        "DV_PLATFORM_WINDOWS"
-    }
+	filter "configurations:Release"
+		defines "DV_RELEASE"
+		runtime "Release"
+		optimize "on"
 
-    filter "configurations:Debug"
-        defines "DV_DEBUG"
-        symbols "On"
-
-    filter "configurations:Release"
-        defines "DV_RELEASE"
-        optimize "On"
-
-    filter "configurations:Dist"
-        defines "DV_DIST"
-        optimize "On"
+	filter "configurations:Dist"
+		defines "DV_DIST"
+		runtime "Release"
+		optimize "on"
